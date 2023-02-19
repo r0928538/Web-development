efe5e3c9259d0f10c5c42e4755515a13c724535a
r0928538 <luiza.lysiak@student.vives.be>
2023-02-19 16:02:54 +0100

    les2

---
+++ les2/scripts/opdracht_substring.js
@@ -0,0 +1,19 @@
+const setup = () => {
+    let btnSubstring=document.getElementById("btnSubstring");
+    btnSubstring.addEventListener("click", substringBerekenen);
+}
+
+const substringBerekenen = () => {
+    let txtInput=document.getElementById("txtInput");
+    let txtOutput=document.getElementById("txtOutput");
+    let txtLinks=document.getElementById("txtLinks");
+    let txtRechts=document.getElementById("txtRechts");
+    let links=parseInt(txtLinks.value, 10);
+    let rechts=parseInt(txtRechts.value, 10);
+    console.log(txtInput);
+    let tekst=txtInput.value;
+    tekst = tekst.substring(links, rechts);
+    txtOutput.innerHTML= tekst;
+}
+
+window.addEventListener('load', setup);
