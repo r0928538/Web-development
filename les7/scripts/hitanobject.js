eb28145b8b4175b2f2a4b3e8451ebe5584daf926
r0928538 <luiza.lysiak@student.vives.be>
2023-04-01 16:03:37 +0200

    les7

---
+++ les7/scripts/hitanobject.js
@@ -0,0 +1,136 @@
+let global = {
+    IMAGE_COUNT: 5,  // aantal figuren
+    IMAGE_SIZE: 48, // grootte van de figuur
+    IMAGE_PATH_PREFIX: "../images/hitanobject",  // map van de figuren
+    IMAGE_PATH_SUFFIX: ".png",  // extensie van de figuren
+    MOVE_DELAY: 3000, // aantal milliseconden voor een nieuwe afbeelding verschijnt
+    score: 0,    // aantal hits
+    timeoutId: 0, // id van de timeout timer, zodat we die kunnen annuleren
+    running: true
+}
+
+let imgAlts = {
+    IMAGE_0: "bom",
+    IMAGE_1: "donut",
+    IMAGE_2: "fruitmuffin",
+    IMAGE_3: "appeltaart",
+    IMAGE_4: "chocotaart"
+}
+
+const setup = () => {
+    maakStartButton();
+}
+
+const maakStartButton = () => {
+    let aantalknopen = document.querySelectorAll("button").length;
+    if(aantalknopen < 1){
+        let start = document.createElement("button");
+        start.textContent = "Start";
+        start.style.width = "50px";
+        start.style.height = "20px";
+        let startEnScore = document.querySelectorAll("div");
+        startEnScore = startEnScore[1];
+        startEnScore.appendChild(start);
+        start.addEventListener("click", starten);
+    }
+}
+
+const starten = () => {
+    deleteStart();
+    let score = document.querySelector("#score");
+    global.score = 0;
+    global.running = true;
+    score.textContent = global.score;
+    speel();
+}
+
+const speel = () => {
+    let img = document.querySelector("img");
+    img.addEventListener("click", geklikt);
+    global.timeoutId = setTimeout(verplaats, global.MOVE_DELAY);
+}
+
+const deleteStart = () => {
+    let button = document.querySelector("button");
+    button.remove();
+}
+
+const geklikt = () => {
+    if(isBom()){
+        if(global.running){
+            alert("Game over");
+            global.running = false;
+            clearTimeout(global.timeoutId);
+        }
+    }
+    else {
+        verplaats();
+        global.score++;
+        let score = document.querySelector("#score");
+        score.textContent = global.score;
+        clearTimeout(global.timeoutId);
+        global.timeoutId = setTimeout(verplaats, global.MOVE_DELAY);
+    }
+}
+
+const verplaats = () => {
+    changeImage();
+    moveImage();
+}
+
+const isBom = () => {
+    let img = document.querySelector("img");
+    if(img.alt === "bom"){
+        maakStartButton();
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const changeImage = () => {
+    let randomimage = Math.random() * global.IMAGE_COUNT;
+    randomimage = Math.floor(randomimage);
+
+    let img = document.querySelector("img");
+    img.src = global.IMAGE_PATH_PREFIX + randomimage + global.IMAGE_PATH_SUFFIX;
+    img.alt = getAlt(randomimage);
+    img.style.width = global.IMAGE_SIZE;
+    img.style.height = global.IMAGE_SIZE;
+}
+
+const moveImage = () => {
+    let img = document.querySelector("img");
+    let canvas=document.querySelector("div");
+    let maxLeft=canvas.clientWidth - img.offsetWidth;
+    let maxHeight=canvas.clientHeight - img.offsetHeight;
+    let left= Math.floor(Math.random()*maxLeft) + "px";
+    let top= Math.floor(Math.random()*maxHeight) + "px";
+    img.style.left=left;
+    img.style.top=top;
+}
+
+const getAlt = (randomimage) => {
+    let imgalt = "";
+    if(randomimage === 0){
+        imgalt = imgAlts.IMAGE_0;
+    }
+    else if(randomimage === 1){
+        imgalt = imgAlts.IMAGE_1;
+    }
+    else if(randomimage === 2){
+        imgalt = imgAlts.IMAGE_2;
+    }
+    else if(randomimage === 3){
+        imgalt = imgAlts.IMAGE_3;
+    }
+    else if(randomimage === 4){
+        imgalt = imgAlts.IMAGE_4;
+    }
+    return imgalt;
+}
+
+window.addEventListener("load", setup);
+
+
---
+++ les7/scripts/memorygame.js
@@ -0,0 +1,166 @@
+let global = {
+    AANTAL_HORIZONTAAL:4,
+    AANTAL_VERTICAAL:3,
+    AANTAL_KAARTEN:6,
+    kaartnamen: ["achterkant", "kaart1", "kaart2", "kaart3", "kaart4", "kaart5", "kaart6"],
+    timeoutId: 0,
+    omgedraait: []
+}
+
+const setup = () => {
+    plaatsRijen();
+    plaatsKaarten();
+}
+
+const toonKaart = (event) => {
+    event.target.src = "../images/" + event.target.id + ".jpg";
+    let kaart = event.target;
+    event.target.alt = altChecker(kaart);
+    if(global.omgedraait.length === 1){
+        alleKaartenUclickable();
+        global.omgedraait.push(kaart);
+        setTimeout(verwijderMatch, 10);
+    }
+    else{
+        global.omgedraait.push(kaart);
+    }
+    console.log(global.omgedraait);
+}
+
+const verwijderMatch = () => {
+    let id1 = global.omgedraait[0].id;
+    let id2 = global.omgedraait[1].id;
+    if(id1 === id2) {
+        setTimeout(blinkGroen, 10);
+    }
+    else{
+        setTimeout(hideKaart, 1000);
+    }
+}
+
+
+const blinkGroen = () => {
+    global.omgedraait[0].style.border = "10px solid green";
+    global.omgedraait[1].style.border = "10px solid green";
+    setTimeout(deleteMatchKaarten, 1000);
+}
+
+const hideKaart = () => {
+    global.omgedraait[0].alt = global.kaartnamen[0];
+    global.omgedraait[0].src = "../images/" + global.kaartnamen[0] + ".jpg";
+    global.omgedraait[1].alt = global.kaartnamen[0];
+    global.omgedraait[1].src = "../images/" + global.kaartnamen[0] + ".jpg";
+    global.omgedraait = [];
+    setTimeout(alleKaartenClickable, 100);
+}
+
+const deleteMatchKaarten = () => {
+    let leeg1 = document.createElement("div");
+    let leeg2 = document.createElement("div");
+    leeg1.className = "leegruimte";
+    leeg2.className = "leegruimte";
+    let parent = global.omgedraait[0].parentElement;
+    let parent2 = global.omgedraait[1].parentElement;
+    parent.insertBefore(leeg1,global.omgedraait[0]);
+    parent2.insertBefore(leeg2,global.omgedraait[1]);
+    global.omgedraait[0].remove();
+    global.omgedraait[1].remove();
+    global.omgedraait = [];
+    setTimeout(alleKaartenClickable, 100);
+}
+
+const alleKaartenUclickable = () => {
+    let kaarten = document.querySelectorAll("img");
+    for(let i = 0; i < kaarten.length; i++){
+        kaarten[i].style.pointerEvents = "none";
+    }
+}
+
+const alleKaartenClickable = () => {
+    let kaarten = document.querySelectorAll("img");
+    if(kaarten.length === 0){
+        alert("Goed gedaan!!!!!! Je hebt alle kaarten gevonden.");
+    }
+    else{
+        for(let i = 0; i < kaarten.length; i++){
+            kaarten[i].style.pointerEvents = "auto";
+        }
+    }
+}
+
+const altChecker = (kaart) => {
+    let id = kaart.id;
+    if(id === global.kaartnamen[1]){
+        return "kat";
+    }
+    else if(id === global.kaartnamen[2]){
+        return "muis";
+    }
+    else if(id === global.kaartnamen[3]){
+        return "konijn";
+    }
+    else if(id === global.kaartnamen[4]){
+        return "vis";
+    }
+    else if(id === global.kaartnamen[5]){
+        return "kikker";
+    }
+    else if(id === global.kaartnamen[6]){
+        return "papegaai";
+    }
+    else{
+        return "onbekend";
+    }
+}
+
+const plaatsRijen = () => {
+    let body = document.querySelector("body");
+    for(let i = 0; i < global.AANTAL_VERTICAAL; i++){
+        let div = document.createElement("div");
+        body.appendChild(div);
+    }
+}
+
+const plaatsKaarten = () => {
+    let i = 0;
+    let aantalgeplaats = [];
+    for(let j = 1; j <= global.AANTAL_KAARTEN; j++){
+        aantalgeplaats.push(0);
+    }
+    while(i < (global.AANTAL_KAARTEN)){
+        let randomkaart = Math.random() * global.AANTAL_KAARTEN;
+        randomkaart = Math.floor(randomkaart);
+        if(aantalgeplaats[randomkaart] === 1){
+            aantalgeplaats[randomkaart]++;
+            plaatsKaart(randomkaart);
+            i++;
+        }
+        else if(aantalgeplaats[randomkaart] === 0){
+            aantalgeplaats[randomkaart]++;
+            plaatsKaart(randomkaart);
+        }
+    }
+}
+
+const plaatsKaart = (randomkaart) => {
+    let canvas = document.querySelectorAll("div");
+    let kaart = document.createElement("img");
+    kaart.alt = "kaart";
+    kaart.id = "kaart" + (randomkaart + 1);
+    kaart.src = "../images/" + global.kaartnamen[0] + ".jpg";
+    let gelukt = false;
+    let i = 0;
+    while(!gelukt){
+        if(canvas[i].children.length < global.AANTAL_HORIZONTAAL){
+            canvas[i].appendChild(kaart);
+            gelukt = true;
+        }
+        else{
+            i++;
+        }
+    }
+    kaart.addEventListener("click", toonKaart);
+}
+
+
+window.addEventListener("load", setup);
